a11cd822f738511fe0740f634f70b8a160a22f79
Evgenia Chendeva <evgenia.chendeva@proton.me>
2023-05-11 16:45:01 +0300

    Entity Group update: now we have Many-To-Many relation between groups and courses; group have Set<Course> courses, also table groups_courses_relation added

--- universitycms/src/main/java/com/foxminded/universitycms/entity/Group.java
+++ universitycms/src/main/java/com/foxminded/universitycms/entity/Group.java
@@ -6,10 +6,15 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+import java.util.Set;
 
 @Builder
 @AllArgsConstructor
@@ -25,4 +30,13 @@ public class Group {
 
     @Column(name = "group_name")
     private String groupName;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "groups_courses_relation",
+            schema = "university",
+            joinColumns = @JoinColumn(name = "group_id"),
+            inverseJoinColumns = @JoinColumn(name = "course_id")
+    )
+    private Set<Course> courses;
 }
