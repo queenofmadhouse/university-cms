09fe35698ea0302674740d747642e7f7a07f0e60
Evgenia Chendeva <evgenia.chendeva@proton.me>
2023-05-21 20:46:42 +0300

    @IntegrationTest annotation created

---
+++ universitycms/src/test/java/com/foxminded/universitycms/testconfiguration/IntegrationTest.java
@@ -0,0 +1,29 @@
+package com.foxminded.universitycms.testconfiguration;
+
+import com.foxminded.universitycms.repository.CourseRepository;
+import com.foxminded.universitycms.repository.GroupRepository;
+import com.foxminded.universitycms.repository.ScheduleRepository;
+import com.foxminded.universitycms.service.impl.ScheduleServiceImpl;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.test.annotation.DirtiesContext;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@DataJpaTest(includeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {
+        ScheduleRepository.class,
+        ScheduleServiceImpl.class,
+        CourseRepository.class,
+        GroupRepository.class
+}))
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+public @interface IntegrationTest {
+}
