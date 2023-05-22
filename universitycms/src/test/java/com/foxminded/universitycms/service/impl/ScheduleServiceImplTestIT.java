93baadc9098d2d7f419c1ecff60deae6a9f24c7a
Evgenia Chendeva <evgenia.chendeva@proton.me>
2023-05-23 02:44:01 +0300

    ScheduleServiceImplTestIT update: now we use pre-created test data by sql-script

--- universitycms/src/test/java/com/foxminded/universitycms/service/impl/ScheduleServiceImplTestIT.java
+++ universitycms/src/test/java/com/foxminded/universitycms/service/impl/ScheduleServiceImplTestIT.java
@@ -1,35 +1,31 @@
 package com.foxminded.universitycms.service.impl;
 
-import com.foxminded.universitycms.entity.Course;
 import com.foxminded.universitycms.entity.Group;
 import com.foxminded.universitycms.entity.Schedule;
 import com.foxminded.universitycms.entity.Teacher;
-import com.foxminded.universitycms.repository.CourseRepository;
 import com.foxminded.universitycms.repository.GroupRepository;
-import com.foxminded.universitycms.repository.ScheduleRepository;
 import com.foxminded.universitycms.repository.TeacherRepository;
 import com.foxminded.universitycms.testconfiguration.IntegrationTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.jdbc.Sql;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @IntegrationTest
+@Sql(
+        scripts = "/sql/schedule-test-data.sql",
+        executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD
+)
 class ScheduleServiceImplTestIT {
 
     @Autowired
-    ScheduleServiceImpl scheduleService;
-
-    @Autowired
-    private ScheduleRepository scheduleRepository;
-
-    @Autowired
-    private CourseRepository courseRepository;
+    private ScheduleServiceImpl scheduleService;
 
     @Autowired
     private GroupRepository groupRepository;
@@ -40,140 +36,26 @@ class ScheduleServiceImplTestIT {
     @Test
     void getScheduleByGroupShouldReturnMapWithScheduleForGroup() {
 
-        LocalDateTime firstDateStart = LocalDateTime.now();
-        LocalDateTime firstDateEnd = firstDateStart.plusHours(1);
+        Group foundGroup = groupRepository.findByGroupId(1L).orElse(null);
 
-        LocalDateTime secondDateStart = firstDateStart.plusDays(5);
-        LocalDateTime secondDateEnd = secondDateStart.plusHours(1);
+        Map<LocalDate,List<Schedule>> fundedSchedule = scheduleService.getScheduleByGroup(foundGroup, 30);
 
-        Teacher teacherAlex = Teacher.builder()
-                .userId(1L)
-                .firstName("Alex")
-                .lastName("Koperfild")
-                .email("mail@mail.mail")
-                .password("123@345")
-                .build();
-
-        Course courseMath = Course.builder()
-                .courseId(1L)
-                .courseName("Math")
-                .courseDescription("Hard")
-                .build();
-
-        Course courseBiology = Course.builder()
-                .courseId(2L)
-                .courseName("Biology")
-                .courseDescription("Hard")
-                .build();
-
-        Group groupA5 = Group.builder()
-                .groupId(1L)
-                .groupName("A5").build();
-
-        teacherRepository.save(teacherAlex);
-        courseRepository.save(courseMath);
-        courseRepository.save(courseBiology);
-        groupRepository.save(groupA5);
-
-        Schedule firstTestSchedule = Schedule.builder()
-                .scheduleId(1L)
-                .teacher(teacherAlex)
-                .course(courseBiology)
-                .group(groupA5)
-                .lessonStart(firstDateStart)
-                .lessonEnd(firstDateEnd)
-                .lessonDescription("Description")
-                .build();
-
-        Schedule secondTestSchedule = Schedule.builder()
-                .scheduleId(2L)
-                .teacher(teacherAlex)
-                .course(courseBiology)
-                .group(groupA5)
-                .lessonStart(secondDateStart)
-                .lessonEnd(secondDateEnd)
-                .lessonDescription("Description")
-                .build();
-
-        scheduleRepository.save(firstTestSchedule);
-        scheduleRepository.save(secondTestSchedule);
-
-        Map<LocalDate,List<Schedule>> fundedSchedule = scheduleService.getScheduleByGroup(groupA5, 30);
-
+        assertNotNull(fundedSchedule);
         assertEquals(2, fundedSchedule.size());
         assertEquals(1, fundedSchedule.get(LocalDate.now()).size());
-        assertEquals(firstTestSchedule, fundedSchedule.get(LocalDate.now()).get(0));
         assertEquals(1, fundedSchedule.get(LocalDate.now().plusDays(5)).size());
-        assertEquals(secondTestSchedule, fundedSchedule.get(LocalDate.now().plusDays(5)).get(0));
     }
 
     @Test
     void getScheduleByTeacherShouldReturnMapWithScheduleForTeacher() {
 
-        LocalDateTime firstDateStart = LocalDateTime.now();
-        LocalDateTime firstDateEnd = firstDateStart.plusHours(1);
+        Teacher foundTeacher = teacherRepository.findByUserId(1L).orElse(null);
 
-        LocalDateTime secondDateStart = firstDateStart.plusDays(5);
-        LocalDateTime secondDateEnd = secondDateStart.plusHours(1);
+        Map<LocalDate,List<Schedule>> fundedSchedule = scheduleService.getScheduleByTeacher(foundTeacher, 30);
 
-        Teacher teacherAlex = Teacher.builder()
-                .userId(1L)
-                .firstName("Alex")
-                .lastName("Koperfild")
-                .email("mail@mail.mail")
-                .password("123@345")
-                .build();
-
-        Course courseMath = Course.builder()
-                .courseId(1L)
-                .courseName("Math")
-                .courseDescription("Hard")
-                .build();
-
-        Course courseBiology = Course.builder()
-                .courseId(2L)
-                .courseName("Biology")
-                .courseDescription("Hard")
-                .build();
-
-        Group groupA5 = Group.builder()
-                .groupId(1L)
-                .groupName("A5").build();
-
-        teacherRepository.save(teacherAlex);
-        courseRepository.save(courseMath);
-        courseRepository.save(courseBiology);
-        groupRepository.save(groupA5);
-
-        Schedule firstTestSchedule = Schedule.builder()
-                .scheduleId(1L)
-                .teacher(teacherAlex)
-                .course(courseBiology)
-                .group(groupA5)
-                .lessonStart(firstDateStart)
-                .lessonEnd(firstDateEnd)
-                .lessonDescription("Description")
-                .build();
-
-        Schedule secondTestSchedule = Schedule.builder()
-                .scheduleId(2L)
-                .teacher(teacherAlex)
-                .course(courseBiology)
-                .group(groupA5)
-                .lessonStart(secondDateStart)
-                .lessonEnd(secondDateEnd)
-                .lessonDescription("Description")
-                .build();
-
-        scheduleRepository.save(firstTestSchedule);
-        scheduleRepository.save(secondTestSchedule);
-
-        Map<LocalDate,List<Schedule>> fundedSchedule = scheduleService.getScheduleByTeacher(teacherAlex, 30);
-
+        assertNotNull(fundedSchedule);
         assertEquals(2, fundedSchedule.size());
         assertEquals(1, fundedSchedule.get(LocalDate.now()).size());
-        assertEquals(firstTestSchedule, fundedSchedule.get(LocalDate.now()).get(0));
         assertEquals(1, fundedSchedule.get(LocalDate.now().plusDays(5)).size());
-        assertEquals(secondTestSchedule, fundedSchedule.get(LocalDate.now().plusDays(5)).get(0));
     }
 }
