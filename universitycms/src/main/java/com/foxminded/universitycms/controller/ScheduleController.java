0045e267056edd121e056c1fd0b6728e16c5f6b8
Evgenia Chendeva <evgenia.chendeva@proton.me>
2023-05-29 21:57:35 +0300

    added support of basic description for the lesson also added new mapping to controller to work with lesson's description's

--- universitycms/src/main/java/com/foxminded/universitycms/controller/ScheduleController.java
+++ universitycms/src/main/java/com/foxminded/universitycms/controller/ScheduleController.java
@@ -11,6 +11,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -28,12 +30,18 @@ public class ScheduleController {
     @GetMapping("/schedule")
     public String getStudentSchedule(Model model) {
 
-        Group group = studentService.findById(2).getGroup();
+        Group group = studentService.findById(4).getGroup();
 
         Map<LocalDate, List<Schedule>> schedules = scheduleService.getScheduleByGroup(group, 30);
         List<List<Day>> weeks = calendarCreater.prepareCalendar(schedules);
         model.addAttribute("weeks", weeks);
         return "schedule";
+    }
+
+    @ResponseBody
+    @GetMapping("/schedule/lesson/{scheduleId}")
+    public Schedule getLesson(@PathVariable long scheduleId) {
+        return scheduleService.getScheduleById(scheduleId);
     }
 }
 
