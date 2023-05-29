2b8b278d25f3cd150c781769bfd559562b2c6bb7
Evgenia Chendeva <evgenia.chendeva@proton.me>
2023-05-30 02:50:07 +0300

    + ApplicationConfiguration update

--- universitycms/src/main/java/com/foxminded/universitycms/configuration/ApplicationConfiguration.java
+++ universitycms/src/main/java/com/foxminded/universitycms/configuration/ApplicationConfiguration.java
@@ -11,6 +11,26 @@ public class ApplicationConfiguration {
     private static final int CALENDAR_WEEK_SIZE = 6;
 
     @Bean
+    public int amountOfTeachers() {
+        return 40;
+    }
+
+    @Bean
+    public int amountOfStudents() {
+        return 400;
+    }
+
+    @Bean
+    public int amountOfGroups() {
+        return 30;
+    }
+
+    @Bean
+    public int amountOfCourses() {
+        return 20;
+    }
+
+    @Bean
     public CalendarCreater calendarCreater() {
         return new CalendarCreater(CALENDAR_AMOUNT_OF_DAYS, CALENDAR_WEEK_SIZE);
     }
