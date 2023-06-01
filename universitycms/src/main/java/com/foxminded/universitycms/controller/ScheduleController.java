7ddc56b3a9e2a0fddd6156236420460744e935bc
Evgenia Chendeva <evgenia.chendeva@proton.me>
2023-06-01 03:46:11 +0300

    Update in controllers

--- universitycms/src/main/java/com/foxminded/universitycms/controller/ScheduleController.java
+++ universitycms/src/main/java/com/foxminded/universitycms/controller/ScheduleController.java
@@ -1,24 +1,38 @@
 package com.foxminded.universitycms.controller;
 
+import com.foxminded.universitycms.calendar.CalendarCreater;
+import com.foxminded.universitycms.calendar.Day;
+import com.foxminded.universitycms.dto.GroupDTO;
+import com.foxminded.universitycms.dto.ScheduleDTO;
+import com.foxminded.universitycms.entity.Course;
 import com.foxminded.universitycms.entity.Group;
 import com.foxminded.universitycms.entity.Schedule;
 import com.foxminded.universitycms.entity.Teacher;
+import com.foxminded.universitycms.service.CourseService;
+import com.foxminded.universitycms.service.GroupService;
 import com.foxminded.universitycms.service.ScheduleService;
 import com.foxminded.universitycms.service.StudentService;
 import com.foxminded.universitycms.service.TeacherService;
-import com.foxminded.universitycms.calendar.CalendarCreater;
-import com.foxminded.universitycms.calendar.Day;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
+@Slf4j
 @RequiredArgsConstructor
 @Controller
 public class ScheduleController {
@@ -26,15 +40,19 @@ public class ScheduleController {
     private final ScheduleService scheduleService;
     private final StudentService studentService;
     private final TeacherService teacherService;
+    private final GroupService groupService;
+    private final CourseService courseService;
     private final CalendarCreater calendarCreater;
 
     @GetMapping("/studentschedule")
     public String getStudentSchedule(Model model) {
 
-        Group group = studentService.findById(81).getGroup();
+        Group group = studentService.findById(81).getGroup(); // #TODO: should be changed to REAL userId
 
         Map<LocalDate, List<Schedule>> schedules = scheduleService.getScheduleByGroup(group, 30);
         List<List<Day>> weeks = calendarCreater.prepareCalendar(schedules);
+        List<LocalDate> month = calendarCreater.prepareDates(30);
+        model.addAttribute("month", month);
         model.addAttribute("weeks", weeks);
         return "studentschedule";
     }
@@ -42,12 +60,40 @@ public class ScheduleController {
     @GetMapping("/teacherschedule")
     public String getTeacherSchedule(Model model) {
 
-        Teacher teacher = teacherService.findByUserId(1L);
+        Teacher teacher = teacherService.findByUserId(2L); // #TODO: should be changed to REAL userId
 
         Map<LocalDate, List<Schedule>> schedules = scheduleService.getScheduleByTeacher(teacher, 30);
         List<List<Day>> weeks = calendarCreater.prepareCalendar(schedules);
+        List<Course> courses = new ArrayList<>(teacher.getCourses());
+        model.addAttribute("courses", courses);
         model.addAttribute("weeks", weeks);
         return "teacherschedule";
+    }
+
+    @ResponseBody
+    @GetMapping("/getGroups")
+    public List<GroupDTO> getGroupsRelatedToCourse(@RequestParam("course") long selectedCourse) {
+
+        Course course = courseService.findById(selectedCourse);
+
+        List<Group> groupsRelatedToCourse = groupService.findAllByCourse(course);
+
+        return groupsRelatedToCourse.stream()
+                .map(group -> new GroupDTO(group.getGroupId(), group.getGroupName()))
+                .collect(Collectors.toList());
+    }
+
+    @ResponseBody
+    @GetMapping("/getTimeSlots/{selectedDate}/{teacherId}/{groupId}")
+    public List<LocalTime> getAvailableTimes(
+            @PathVariable LocalDate selectedDate, @PathVariable Long teacherId, @PathVariable long groupId) {
+
+        Teacher teacher = teacherService.findByUserId(teacherId);
+        Group group = groupService.findById(groupId);
+
+        log.info(scheduleService.findFreeTimeForTeacherAndGroup(selectedDate, teacher, group).toString());
+
+        return scheduleService.findFreeTimeForTeacherAndGroup(selectedDate, teacher, group);
     }
 
     @ResponseBody
@@ -55,5 +101,22 @@ public class ScheduleController {
     public Schedule getLesson(@PathVariable long scheduleId) {
         return scheduleService.getScheduleById(scheduleId);
     }
-}
 
+    @ResponseBody
+    @PostMapping(path = "/add", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<ScheduleDTO> addLesson(@RequestBody ScheduleDTO scheduleDTO) {
+
+        Schedule schedule = Schedule.builder()
+                .teacher(teacherService.findByUserId(scheduleDTO.getTeacher()))
+                .course(courseService.findById(scheduleDTO.getCourse()))
+                .group(groupService.findById(scheduleDTO.getGroup()))
+                .lessonStart(scheduleDTO.getLessonStart())
+                .lessonEnd(scheduleDTO.getLessonEnd())
+                .lessonDescription(scheduleDTO.getLessonDescription())
+                .build();
+
+        scheduleService.save(schedule);
+
+        return ResponseEntity.ok(scheduleDTO);
+    }
+}
