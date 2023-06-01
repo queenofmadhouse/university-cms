d713e5cac38372266aa8b8a00dd02d5aa57aef87
Evgenia Chendeva <evgenia.chendeva@proton.me>
2023-06-01 03:44:03 +0300

    web config created

---
+++ universitycms/src/main/java/com/foxminded/universitycms/configuration/WebConfiguration.java
@@ -0,0 +1,18 @@
+package com.foxminded.universitycms.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(String.class, LocalDate.class,
+                source -> LocalDate.parse(source, DateTimeFormatter.ISO_LOCAL_DATE));
+    }
+}
