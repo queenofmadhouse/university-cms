df2e3ad75c979b122c7ef4824be4bf0a03e03cb6
Evgenia Chendeva <evgenia.chendeva@proton.me>
2023-11-13 18:58:24 +0200

    LoginControllerTest refactor: Achieve 100% coverage in LoginControllerTest

--- universitycms/src/test/java/com/foxminded/universitycms/controller/LoginControllerTest.java
+++ universitycms/src/test/java/com/foxminded/universitycms/controller/LoginControllerTest.java
@@ -1,16 +1,21 @@
 package com.foxminded.universitycms.controller;
 
+import com.foxminded.universitycms.testconfiguration.IntegrationTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
-@WebMvcTest(LoginController.class)
+@IntegrationTest
+@AutoConfigureMockMvc
+@Transactional
 class LoginControllerTest {
 
     @Autowired
@@ -18,9 +23,9 @@ class LoginControllerTest {
 
     @Test
     @WithMockUser(roles = "TEACHER", username = "first@mail.com")
-    void getHomePageShouldReturnHomePage() throws Exception {
-        mockMvc.perform(get("/login")
-                        .with(csrf()))
-                .andExpect(status().isOk());
+    void getLoginPageShouldReturnLoginPage() throws Exception {
+        mockMvc.perform(get("/login").with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(view().name("login"));
     }
 }
